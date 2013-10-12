c8bff7f2d15e620d1bb2ca8dc53603b2dee73494
gzp <gzp9595@gmail.com>
2013-10-12 13:57:15 +0800

    将 L U D F B 修改了  还有Y Z改了 当时忽略了面的标号

--- Cube_Turn.h
+++ Cube_Turn.h
@@ -37,8 +37,8 @@ int Turn_R()
 {
 	char a[3];
 	a[0]=up[0][2];a[1]=up[1][2];a[2]=up[2][2];
-	up[0][2]=back[0][2];up[1][2]=back[1][2];up[2][2]=back[2][2];
-	back[0][2]=down[0][2];back[1][2]=down[1][2];back[2][2]=down[2][2];
+	up[0][2]=back[2][0];up[1][2]=back[1][0];up[2][2]=back[0][0];
+	back[2][0]=down[0][2];back[1][0]=down[1][2];back[0][0]=down[2][2];
 	down[0][2]=front[0][2];down[1][2]=front[1][2];down[2][2]=front[2][2];
 	front[0][2]=a[0];front[1][2]=a[1];front[2][2]=a[2];
 	Turn_M(right);	
@@ -57,8 +57,8 @@ int Turn_L()
 	a[0]=up[0][0];a[1]=up[1][0];a[2]=up[2][0];
 	up[0][0]=front[0][0];up[1][0]=front[1][0];up[2][0]=front[2][0];
 	front[0][0]=down[0][0];front[1][0]=down[1][0];front[2][0]=down[2][0];
-	down[0][0]=back[0][0];down[1][0]=back[1][0];down[2][0]=back[2][0];
-	back[0][0]=a[0];back[1][0]=a[1];back[2][0]=a[2];
+	down[0][0]=back[2][2];down[1][0]=back[1][2];down[2][0]=back[0][2];
+	back[2][2]=a[0];back[1][2]=a[1];back[0][2]=a[2];
 	Turn_M(left);
 }	
 
@@ -111,8 +111,8 @@ int Turn_F()
 	char a[3];
 	Turn_M(front);
 	a[0]=up[0][0];a[1]=up[0][1];a[2]=up[0][2];
-	up[0][0]=left[0][2];up[0][1]=left[1][2];up[0][2]=left[2][2];
-	left[0][2]=down[2][0];left[1][2]=down[2][1];left[2][2]=down[2][2];
+	up[0][0]=left[2][0];up[0][1]=left[1][0];up[0][2]=left[0][0];
+	left[2][0]=down[2][0];left[1][0]=down[2][1];left[0][0]=down[2][2];
 	down[2][0]=right[0][0];down[2][1]=right[0][1];down[2][2]=right[2][0];
 	right[0][0]=a[0];right[1][0]=a[1];right[2][0]=a[2];
 }
@@ -129,9 +129,9 @@ int Turn_B()
 	char a[3];
 	Turn_M(back);
 	a[0]=up[2][0];a[1]=up[2][1];a[2]=up[2][2];
-	up[2][0]=left[0][0];up[2][1]=left[1][0];up[2][2]=left[2][0];
-	left[0][0]=down[0][0];left[1][0]=down[0][1];left[2][0]=down[0][2];
-	down[0][0]=right[0][2];down[0][1]=right[0][2];down[0][2]=right[2][2];
+	up[2][0]=left[2][2];up[2][1]=left[1][2];up[2][2]=left[0][2];
+	left[2][2]=down[0][0];left[1][2]=down[0][1];left[0][2]=down[0][2];
+	down[0][0]=right[0][2];down[0][1]=right[1][2];down[0][2]=right[2][2];
 	right[0][2]=a[0];right[1][2]=a[1];right[2][2]=a[2];
 }
 
@@ -160,11 +160,14 @@ int Turn_x()
 
 int Turn_Y()
 {
-	Turn_Swap(up,left);
-	Turn_Swap(left,down);
-	Turn_Swap(down,right);
-	Turn_M(front);
-	Turn_m(back);
+	Turn_F();
+	char a[3];
+	a[0]=up[1][0];a[1]=up[1][1];a[2]=up[1][2];
+	up[1][0]=right[2][1];up[1][1]=right[1][1];up[1][2]=right[0][1];
+	right[2][1]=down[1][2];right[1][1]=down[1][1];right[0][1]=down[1][0];
+	down[1][2]=left[0][1];down[1][1]=left[1][1];down[1][0]=left[2][1];
+	left[0][1]=a[0];left[1][1]=a[1];left[2][1]=a[2];
+	Turn_b();
 }
 
 int Turn_y()
@@ -176,11 +179,14 @@ int Turn_y()
 
 int Turn_Z()
 {
-	Turn_Swap(up,front);
-	Turn_Swap(front,down);
-	Turn_Swap(down,back);
-	Turn_M(right);
-	Turn_m(left);
+	Turn_R();
+	Turn_l();
+	char a[3];
+	a[0]=up[0][1];a[1]=up[1][1];a[2]=up[2][1];
+	up[0][1]=back[2][1];up[1][1]=back[1][1];up[2][1]=back[0][1];
+	back[2][1]=down[0][1];back[1][1]=down[1][1];back[0][1]=down[2][1];
+	down[0][1]=front[0][1];down[1][1]=front[1][1];down[2][1]=front[2][1];
+	front[0][1]=a[0];front[1][1]=a[1];front[2][1]=a[2];
 }
 
 int Turn_z()
