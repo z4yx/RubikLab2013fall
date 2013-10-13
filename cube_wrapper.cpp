bc42df06b4258d05331131425eac1c7314fba26a
张宇翔 <zz593141477@gmail.com>
2013-10-13 13:17:25 +0800

    合并gzp的第二层算法

--- cube_wrapper.cpp
+++ cube_wrapper.cpp
@@ -2,7 +2,7 @@
 #include <Python/Python.h>
 #include "Cube_Turn.h"
 #include "cube_step_one.h"
-#include "solve_2.cpp"
+#include "second.h"
 #include "Third.h"
 #include "step_final.hpp"
 
@@ -175,8 +175,8 @@ PyObject* step1(PyObject* self, PyObject *args)
 }
 PyObject* step2(PyObject* self, PyObject *args)
 {
-	second second_solver;
-	std::string steps = second_solver.solve_2(cubeObj);
+	Second second_solver;
+	std::string steps = second_solver.Main(cubeObj);
 	printf("step2: %s\n", steps.c_str());
 	return Py_BuildValue("s", steps.c_str());
 }
